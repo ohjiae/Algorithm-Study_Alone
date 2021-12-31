a7f5315d5ca7a7ad82933ce8c4e6a5c44f54c34a
OH JI AE <ohjiae.dev@gmail.com>
2021-12-31 12:22:16 +0900

    Delete 10809_알파벳_찾기.py

--- 10809_알파벳_찾기.py
+++
@@ -1,9 +0,0 @@
-# [10809번 : 알파벳 찾기](https://www.acmicpc.net/problem/10809)
-# 🙋‍♀️ 내 코드
-
-S = input()
-alpha = 'abcdefghijklmnopqrstuvwxyz'
-result = []
-for a in alpha :
-    result.append(S.find(a))
-print(*result, sep = ' ')
